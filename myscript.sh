5dca5d8303d330f1b1b0e08016cb81c987786b2a
Paul Faust <149526228+pfaustLouKy@users.noreply.github.com>
2024-04-18 19:06:01 +0000

    associative arrays

--- myscript.sh
+++ myscript.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-declare -a snacks=("apple" "banana" "orange")
-echo ${snacks[2]}
-snacks[5]="grape"
-snacks+=("mango")
-echo ${snacks[@]}
-for i in {0..6}; do echo "$i: ${snacks[i]}"; done
+declare -a office
+office[city]="San Francisco"
+office["building_name"]="HQ West"
+echo ${office["building_name"]} is in ${office[city]}
