e0b4bc4a5f0e871c70b2be0ceb247b35c214b0a0
Paul Faust <149526228+pfaustLouKy@users.noreply.github.com>
2024-04-18 20:48:04 +0000

    bash colors

--- myscript.sh
+++ myscript.sh
@@ -5,3 +5,21 @@ echo "Free space: $freespace"
 
 freemem=$(free -h | awk 'NR==2 {print $4}')
 echo "Free memory: $freemem"
+
+greentext="\033[32m"
+bold="\033[1m"
+normal="\033[0m"
+
+printf -v logdate "%(%Y-%m-%d)T"
+
+echo -e "$bold Quick system report for $greentext
+$HOSTNAME$normal"
+
+printf "\tKernel Release:\t%s\n" "$(uname -r)"
+printf "\tBash Version:\t%s\n" "$BASH_VERSION"
+printf "\tFree Space:\t%s\n" "$freespace"
+printf "\tFree Memory:\t%s\n" "$freemem"
+printf "\tFiles in pwd:\t%s\n" "$(ls | wc -l)"
+printf "\tGenerated on: \t%s\n" "$logdate"
+
+echo -e "\033[31mBright\033[0m"
