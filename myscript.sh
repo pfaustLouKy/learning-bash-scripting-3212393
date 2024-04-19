ea50050d2c6a57cca5344f92af7bae85abdee35c
Paul Faust <149526228+pfaustLouKy@users.noreply.github.com>
2024-04-19 18:33:13 +0000

    for loop array

--- myscript.sh
+++ myscript.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
 
-echo "While Loop"
-
-declare -i n=0
-while ((n<10))
+declare -a fruits=("apple" "banana" "cherry")
+for i in ${fruits[@]}
 do
-  echo  "n:$n"
-  ((n++))
-done
-
-echo -e "\n Until Loop"
-
-declare -i m=0
-until ((m==10)); do
-  echo "m:$m"
-  ((m++))
+  echo $i
 done
